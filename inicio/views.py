a46df0929b016a4231d4ae4c1f9b828c08e0b130
Albano-Bosquet <bosquet.albano@gmail.com>
2025-02-20 21:23:53 -0300

    Creamos el proyecto con dos vistas

---
+++ inicio/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+
+# Create your views here.
+#request es un objeto que contiene toda la información de la petición web
+def inicio(request):
+    #return HttpResponse({'clave' : 'valor'})
+    #return HttpResponse("Hola soy la vista de inicio")
+    #return HttpResponse('<h1>PAGINA DE INICIO</h1>')
+    return render(request, 'inicio/inicio.html') 
+
+def saludo(request, nombre, apellido):
+    hora_actual = datetime.now()
+    return render(request, 'inicio/saludo.html', {'hora': hora_actual, 'nombre': nombre, 'apellido': apellido})
