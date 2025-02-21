69f9743a4a2a84fb87c4bb8280f4e132f06cf995
Albano-Bosquet <bosquet.albano@gmail.com>
2025-02-20 21:59:52 -0300

    Creamos el primer modelo

--- inicio/models.py
+++ inicio/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Planta:
+    nombre = models.CharField(max_length=50)
+    tipo = models.CharField(max_length=50)
+    descripcion = models.TextField(null=True, blank=True)
+    contenido_thc = models.CharField(max_length=10)
+    contenido_cbd = models.CharField(max_length=10)
+    autofloreciente = models.BooleanField
+    fotoperiodica = models.BooleanField
+    foto = models.ImageField(null=True, blank=True)
