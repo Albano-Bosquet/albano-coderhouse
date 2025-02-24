eae1035a9c23e03aa097b456a4165819d446b9fe
Albano-Bosquet <bosquet.albano@gmail.com>
2025-02-24 19:38:16 -0300

    Se realizaron correcciones, ya se pueden cargar correctamente las imagenes

--- inicio/models.py
+++ inicio/models.py
@@ -8,7 +8,7 @@ class Planta(models.Model):
     contenido_cbd = models.CharField(max_length=5)
     autofloreciente = models.BooleanField(default=False) 
     fotoperiodica = models.BooleanField(default=False) 
-    foto = models.ImageField(null=True, blank=True, upload_to='media/')
+    foto = models.ImageField(null=True, blank=True, upload_to='plantas/')
     
     def __str__(self):
-        return f'{self.nombre}, {self.tipo}\n{self.foto}'
+        return f'{self.nombre}, {self.tipo}'
